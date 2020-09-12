5ff0f8d3675990e93e27804ba703211941d83b29
pangjiaming <reng99@outlook.com>
2020-09-12 12:27:26 +0800

    更改关键字

--- article-project/service/config/config.default.js
+++ article-project/service/config/config.default.js
@@ -42,7 +42,7 @@ module.exports = appInfo => {
     dialect: 'mysql',
     host: '127.0.0.1',
     port: 3306,
-    database: 'instagram',
+    database: 'article',
     username: 'reng',
     password: '123456',
     operatorsAliases: false
